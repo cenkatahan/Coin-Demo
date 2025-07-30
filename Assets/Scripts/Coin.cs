a7c331a4a68778af8fb6a89f8668557654afdff3
Cenk Atahan Özbek <49042414+cenkatahan@users.noreply.github.com>
2025-07-30 13:42:50 +0300

    add coin collision

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,16 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log($"{other.name} collided with: {gameObject.name}");
+            Debug.Log("Coin Gained");
+            
+        }
     }
 }
